9151a3cc9997878e9396f38ce68bc9fe8b828d1e
anton <ant.arhipov@gmail.com>
2023-09-26 11:21:47 +0300

    add setters

--- src/main/java/me/arhan/aidemo/math/D.java
+++ src/main/java/me/arhan/aidemo/math/D.java
@@ -15,4 +15,12 @@ public class D {
     public int getY() {
         return y;
     }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
